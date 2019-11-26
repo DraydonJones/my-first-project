6a169efc2b43620cd19a6945fba07de9364aae5d
Moishe Barnes <moishebarnes@gmail.com>
2019-11-26 20:22:05 +0000

    updated login feature

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 // alert('hello')
 const button = document.getElementById('button')
-const password = document.getElementById('password')
+// const password = document.getElementById('password')
 const username = document.getElementById('username')
 const div1 = document.getElementById('div1')
 const div2 = document.getElementById('div2')
-const h1 = document.createElement('h1')
-
+const  images= []
 
 let usernameValue =''
 
+saveImages()
 
 username.addEventListener('change', saveUsername)
 
@@ -19,12 +19,36 @@ function saveUsername() {
 button.addEventListener('click', login)
 
 function login() {
-  if(usernameValue==='Draydon'){
-    console.log('hello')
-    h1.innerHTML ='Hi'
-div2.appendChild(h1)
-  }else {
-  console.log(usernameValue)
+  if(usernameValue==='Draydon'){ 
+    showImages()
+  } else {
+  
+  alert('wrong user name')
+  }
 }
-  // alert('hello ' + usernameValue)
+
+function saveImages() {
+  fetch('https://jsonplaceholder.typicode.com/photos?_start=0&_limit=5')
+  .then(response => response.json())
+  .then(json => images.push(json))
 }
+
+function showImages() {
+  console.log(images[0][0])
+
+  let i = 0
+  while (i <= 4) {
+    let h1 = document.createElement('h1')
+    let img = document.createElement('img')
+    h1.innerHTML = images[0][i].title
+    div2.appendChild(h1)
+    img.src = images[0][i].url
+    h1.appendChild(img)
+    console.log(img) 
+    console.log(h1) 
+  i++
+  }
+} 
+
+
+
